360c1cd7fae66ccf933112a4596a8d858861eb42
evenhumble <patrickwuke@163.com>
2017-12-28 10:31:21 +0800

    add new euler samples

---
+++ steps/euler/euler01.go
@@ -0,0 +1,34 @@
+/**
+ * Problem 1
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23.
+
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+
+ * Answer: 233168
+**/
+
+package main
+
+import (
+	"fmt"
+)
+
+const MAX=1000
+
+func check(num int) int {
+	if(num%3==0) || (num%5==0){
+		return num
+	}
+	return 0
+}
+
+func main(){
+	sum :=0
+	for i:=0;i<MAX;i++{
+		sum+=check(i)
+	}
+
+	fmt.Println("Total: %d",(sum))
+}
