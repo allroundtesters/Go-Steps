3d489ec8d4d3c3d29b87eb83c5ae251427d43110
patrick <patrickwuke@163.com>
2020-07-15 17:56:32 +0800

    add ont perf

---
+++ practise/blockchain/ont/simpleperf.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+config "./config_ont"
+"fmt"
+config "github.com/mockyz/AutoTestGo/ont-test/config_ont"
+OntTools "github.com/mockyz/AutoTestGo/ont-test/utils"
+goSdk "github.com/ontio/ontology-go-sdk"
+"github.com/ontio/ontology-go-sdk/client"
+"github.com/ontio/ontology/common/log"
+)
+
+var sdk = goSdk.NewOntologySdk()
+
+func main() {
+	log.InitLog(log.InfoLog, log.PATH, log.Stdout)
+	configPath := "ont-test/config.json"
+	cfg, err := config.ParseConfig(configPath)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	wallet, err := sdk.OpenWallet(cfg.Wallet)
+	if err != nil {
+		log.Errorf("parse wallet err: %s", err)
+	}
+	account, err := wallet.GetDefaultAccount([]byte(cfg.Password))
+	if err != nil {
+		log.Errorf("get account err: %s", err)
+	}
+	rpcClient := client.NewRpcClient()
+	rpcClient.SetAddress(cfg.Rpc[0])
+	sdk.SetDefaultClient(rpcClient)
+	OntTools.BindAssetHash(cfg, account)
+	OntTools.GetCrossLimt(cfg, account)
+	OntTools.GetAseetHash(cfg, account)
+
+	//OntTools.TestTransfer(cfg, account)
+	//address,_ :=common.AddressFromBase58("AUeKhaRr9xwy114zwsVarYvVG13C2T3C9o")
+	//OntTools.BalanceOf(sdk,address,cfg.EthX)
+}
+
